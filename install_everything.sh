26ccdf8c7c4d64d417f543bee7bb897188d0e6a0
Søren Valentin Silkjær <soren.hansen@jp.dk>
2023-12-26 14:32:24 +0100

    Install everything script

---
+++ install_everything.sh
@@ -0,0 +1,9 @@
+# Must be run from this folder
+
+sudo apt install iw openconnect lm-sensors fonts-font-awesome alsa-utils i3 i3blocks pip arandr trash-cli vim
+
+
+ln -s git/gitconfig ~/.gitconfig
+ln -s .config/i3 .config/i3
+ln -s .config/i3blocks .config/i3blocks
+
